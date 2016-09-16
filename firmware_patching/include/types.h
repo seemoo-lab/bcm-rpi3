74e903fbf1324590eb64055342206069825da9f8
XePeleato <edu@error404software.com>
2016-09-16 23:15:34 +0200

    Add missing includes
    * Add brcm.h missing file.
    * Add u8 > uint8_t data type.

--- firmware_patching/include/types.h
+++ firmware_patching/include/types.h
@@ -7,6 +7,7 @@ typedef unsigned int            dword;
 typedef unsigned char           bool;
 typedef signed char             int8_t;
 typedef unsigned char           uint8_t;
+typedef unsigned char		u8;
 typedef signed short int        int16_t;
 typedef unsigned short int      uint16_t;
 typedef signed int              int32_t;
---
+++ firmware_patching/monitor_mode/brcm.h
@@ -0,0 +1,3 @@
+#include "../include/types.h"
+#define CHSPEC_CHANNEL(chspec)  ((u8)((chspec) & WL_CHANSPEC_CHAN_MASK))
+#define WL_CHANSPEC_CHAN_MASK           0x00ff
