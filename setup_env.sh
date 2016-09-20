a51afa8f5add249df18b1c8c9ae8ecfb4b0a620a
Mikael Kjaer <mkjaer@users.noreply.github.com>
2016-09-20 18:38:04 +0400

    Update setup_env.sh
    Made the script more robust by changing to the script folder before trying instead of just failing if called from another folder.
    Also added check if both folders being used exists.

--- setup_env.sh
+++ setup_env.sh
@@ -1,17 +1,20 @@
-if [ -d "$(pwd)/buildtools" ]; then
-export CC=$(pwd)/buildtools/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-
-export CROSS_COMPILE=$(pwd)/buildtools/arm-eabi-4.7/bin/arm-eabi-
+#!/bin/bash
+OLD_PWD=$(pwd)
+cd $(dirname ${BASH_SOURCE[0]})
 
-export ARCH=arm
-export KERNEL=kernel7
+if [ -d "$(pwd)/buildtools" ] && [ -d "$(pwd)/firmware_patching" ]; then
+ export CC=$(pwd)/buildtools/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-
+ export CROSS_COMPILE=$(pwd)/buildtools/arm-eabi-4.7/bin/arm-eabi-
 
-export NEXMON_FIRMWARE_PATCHING=$(pwd)/firmware_patching
-export NEXMON_ROOT=$(pwd)
+ export ARCH=arm
+ export KERNEL=kernel7
 
-export NEXMON_SETUP_ENV=1
+ export NEXMON_FIRMWARE_PATCHING=$(pwd)/firmware_patching
+ export NEXMON_ROOT=$(pwd)
 
-else 
-
-echo "This script should be run at nexmon's root folder!"
+ export NEXMON_SETUP_ENV=1
+else
+ echo "One or more required folders are missing!"
 fi
 
+cd "$OLD_PWD"
